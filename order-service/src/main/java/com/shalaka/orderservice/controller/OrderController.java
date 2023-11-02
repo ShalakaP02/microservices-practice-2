b93bff5f554e3ceb1b2bfb62835b1219c2416a8b
ShalakaP02 <shala.p02@gmail.com>
2023-11-02 13:12:49 +0530

    added centralized logging using elk

--- order-service/src/main/java/com/shalaka/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/shalaka/orderservice/controller/OrderController.java
@@ -1,12 +1,16 @@
 package com.shalaka.orderservice.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shalaka.orderservice.common.Payment;
 import com.shalaka.orderservice.common.TransactionRequest;
 import com.shalaka.orderservice.common.TransactionResponse;
 import com.shalaka.orderservice.entity.Order;
 import com.shalaka.orderservice.service.OrderService;
 import org.aspectj.weaver.ast.Or;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,13 +23,21 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @PostMapping("/bookOrder")
-    public TransactionResponse bookOrder(@RequestBody TransactionRequest transactionRequest){
-        return orderService.saveOrder(transactionRequest);
+    public TransactionResponse bookOrder(@RequestBody TransactionRequest transactionRequest) throws JsonProcessingException {
+        log.info("OrderController bookOrder request {} ",new ObjectMapper().writeValueAsString(transactionRequest));
+        TransactionResponse response = orderService.saveOrder(transactionRequest);
+        log.info("OrderController bookOrder response {} ",new ObjectMapper().writeValueAsString(response));
+        return response;
     }
 
     @GetMapping("/getOrder")
-    public List<Order> getOrder(){
-        return orderService.getOrders();
+    public List<Order> getOrder() throws JsonProcessingException {
+        log.info("OrderController getOrder request");
+        List<Order> orderList =  orderService.getOrders();
+        log.info("OrderController getOrder response {}",new ObjectMapper().writeValueAsString(orderList));
+        return orderList;
     }
 }
--- payment-service/src/main/java/com/shalaka/paymentservice/controller/PaymentController.java
+++ payment-service/src/main/java/com/shalaka/paymentservice/controller/PaymentController.java
@@ -1,7 +1,11 @@
 package com.shalaka.paymentservice.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shalaka.paymentservice.entity.Payment;
 import com.shalaka.paymentservice.service.PaymentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,13 +16,21 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    Logger log = LoggerFactory.getLogger(PaymentController.class);
+
     @PostMapping("/doPayment")
-    public Payment doPayment(@RequestBody Payment payment){
-        return paymentService.savePayment(payment);
+    public Payment doPayment(@RequestBody Payment payment) throws JsonProcessingException {
+        log.info("PaymentController doPayment request {} ",new ObjectMapper().writeValueAsString(payment));
+        Payment paymentResp =  paymentService.savePayment(payment);
+        log.info("PaymentController doPayment response {} ",new ObjectMapper().writeValueAsString(paymentResp));
+        return paymentResp;
     }
 
     @GetMapping("/{orderID}")
-    public Payment findPaymentHistoryByOrderID(@PathVariable int orderID){
-        return  paymentService.findPaymentHistoryByOrderID(orderID);
+    public Payment findPaymentHistoryByOrderID(@PathVariable int orderID) throws JsonProcessingException {
+        log.info("PaymentController findPaymentHistoryByOrderID request {} ",orderID);
+        Payment paymentResp = paymentService.findPaymentHistoryByOrderID(orderID);
+        log.info("PaymentController findPaymentHistoryByOrderID response {} ",new ObjectMapper().writeValueAsString(paymentResp));
+        return  paymentResp;
     }
 }
